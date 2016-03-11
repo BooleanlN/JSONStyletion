8a291c844f4c191a677757404799284c762035ff
chenkuan <chenkuan@gigold.com>
2016-03-11 14:44:00 +0800

    完成

--- JSONStyletion.js
+++ JSONStyletion.js
@@ -1,128 +1,129 @@
 /**
  * Created by chenkuan on 16/3/11.
  */
-function styleition(json,key){
-    var indent = '48px';// 缩进宽度
-    var lineMargin = '5px';
-    var styledEle = document.createElement("div");
-    styledEle.style.listStyle = "none";
-    styledEle.style.paddingLeft = "0";
-    styledEle.style.fontFamily = 'Consolas, "Liberation Mono", Menlo, Courier, monospace';
-    var leftq = document.createElement("p"); // 左括弧
-    var wrap = document.createElement("p"); // 值区
-    var rightq = document.createElement("p"); // 右括弧
-    leftq.style.margin = wrap.style.margin = rightq.style.margin = lineMargin+" 0";
-    leftq.style.paddingLeft = wrap.style.paddingLeft = rightq.style.paddingLeft = indent; // 设置值区缩进
-    rightq.style.marginLeft = "0";// 设置回括弧额外缩进
-    styledEle.appendChild(leftq); // 加入左括弧
-    styledEle.appendChild(wrap); // 加入值区
+JSON.constructor.prototype.stylietion = function(){
 
-    // 判断左括弧形式
-    if(json instanceof Array){
-        // 若json是数组
-        leftq.innerHTML="<span style='color:#a71d5d'>\""+key+"\"</span> : [";
-        // 如果key不存在,则不写键名
-        if(!key){
-            leftq.innerHTML="[";
-        }
-        rightq.innerHTML="],";
-    }else{
-        leftq.innerHTML="<span style='color:#a71d5d'>\""+key+"\"</span> : {";
-        // 如果key不存在,则不写键名
-        if(!key){
-            leftq.innerHTML="{";
-        }
-        rightq.innerHTML="},";
-    }
+    return styleition(this,null,true);
 
-    // 求出属性数量
-    var k,length = 0,i = 0;
-    for(k in json){
-        length++;
-    }
+    function styleition(json,key,isLastEle){
+        var indent = '48px';// 缩进宽度
+        var lineMargin = '5px';
+        var styledEle = document.createElement("div");
+        styledEle.style.listStyle = "none";
+        styledEle.style.paddingLeft = "0";
+        styledEle.style.fontFamily = 'Consolas, "Liberation Mono", Menlo, Courier, monospace';
 
-    // 遍历json属性值
-    for(k in json){
-        // 确定当前行数
-        i = i + 1;
+        var leftq = document.createElement("p"); // 左括弧
+        var wrap = document.createElement("p"); // 值区
+        var rightq = document.createElement("p"); // 右括弧
 
-        // 获得键
-        var styledKey = "<span style='color:#a71d5d'>\""+k+"\"</span> : ";
-        // 如果json是数组,则不需要键名
-        console.log(json);
+        leftq.style.margin = wrap.style.margin = rightq.style.margin = lineMargin+" 0";
+        leftq.style.paddingLeft = wrap.style.paddingLeft = rightq.style.paddingLeft = indent; // 设置值区缩进
+        rightq.style.marginLeft = "0";// 设置回括弧额外缩进
+        styledEle.appendChild(leftq); // 加入左括弧
+        styledEle.appendChild(wrap); // 加入值区
 
-        // 如果当前json为数组,则值不带键名
+        // 判断左括弧形式
         if(json instanceof Array){
-            styledKey = "";
-        }
-
-        // 获得值
-        var styledVal = json[k];
-
-        // 数 美化样式
-        if(typeof json[k] == "number"){
-            styledVal = "<span style='color:#0086b3'> "+styledVal+" </span>"
-        }
-
-        // 字符串 美化样式
-        if(typeof json[k] == "string"){
-            styledVal = "<span style='color:#239141'> \""+styledVal+"\" </span>"
-        }
-
-        // 布尔值 美化样式
-        if(typeof json[k] == "boolean"){
-            styledVal = "<span style='color:#a71d5d'> "+styledVal+" </span>"
-        }
-
-        // undefined 美化样式
-        if(typeof json[k] == "undefined"){
-            styledVal = "<span style='color:#969896'> "+styledVal+" </span>"
-        }
-
-        // 对象 递归
-        if(typeof json[k] == "object"){
-            // 若当前元素为数组
-            if(json[k] instanceof Array){
-                // 若父级也为数组
-                if(json instanceof Array){
-                    wrap.appendChild(styleition(json[k]));
-                }else{
-                    wrap.appendChild(styleition(json[k],k));
-                }
-                continue;// 避免当前键值对被重写为 key:[object object] 的形式
-            }else if(json[k]==null){
-                // null值
-                styledVal = "<span style='color:#aa3022'> null </span>"
-            }else{
-                // 若父级也为数组
-                if(json instanceof Array){
-                    wrap.appendChild(styleition(json[k]));
-                }else{
-                    wrap.appendChild(styleition(json[k],k));
-                }
-                continue;// 避免当前键值对被重写为 key:[object object] 的形式
+            // 若json是数组
+            leftq.innerHTML="<span style='color:#a71d5d'>\""+key+"\"</span> : [";
+            // 如果key不存在,则不写键名
+            if(!key){
+                leftq.innerHTML="[";
             }
-        }
-
-        // 新建一行
-        var line = document.createElement("p");
-        // 设置新行缩进和行距
-        line.style.paddingLeft = indent;
-        line.style.margin = lineMargin+" 0";
-
-        // 设置新行值,此处要判断是否到了最后一个对象,来确定是否要加逗号
-        if(i>=length){
-            line.innerHTML = styledKey +styledVal+"<br>";
-            rightq.innerHTML = rightq.innerHTML.replace(",","");
+            rightq.innerHTML= isLastEle ? "]":"],";
         }else{
-            line.innerHTML = styledKey +styledVal+",<br>";
+            leftq.innerHTML="<span style='color:#a71d5d'>\""+key+"\"</span> : {";
+            // 如果key不存在,则不写键名
+            if(!key){
+                leftq.innerHTML="{";
+            }
+            rightq.innerHTML= isLastEle ? "}":"},";
         }
-        // 追加行
-        wrap.appendChild(line);
+
+        // 求出属性数量
+        var k,length = 0,i = 0;
+        for(k in json){
+            if(!json.hasOwnProperty(k))continue;// 排除为json类扩展的方法
+            length++;
+        }
+
+        // 遍历json属性值
+        for(k in json){
+            if(!json.hasOwnProperty(k))continue; // 排除为json类扩展的方法
+            // 确定当前行数
+            i = i + 1;
+
+            // 获得键
+            var styledKey = "<span style='color:#a71d5d'>\""+k+"\"</span> : ";
+
+            // 如果当前json为数组,则值不带键名
+            if(json instanceof Array){
+                styledKey = "";
+            }
+
+            // 获得值
+            var styledVal = json[k];
+
+            // 数 美化样式
+            if(typeof json[k] == "number"){
+                styledVal = "<span style='color:#0086b3'> "+styledVal+" </span>"
+            }
+
+            // 字符串 美化样式
+            if(typeof json[k] == "string"){
+                styledVal = "<span style='color:#239141'> \""+styledVal+"\" </span>"
+            }
+
+            // 布尔值 美化样式
+            if(typeof json[k] == "boolean"){
+                styledVal = "<span style='color:#a71d5d'> "+styledVal+" </span>"
+            }
+
+            // undefined 美化样式
+            if(typeof json[k] == "undefined"){
+                styledVal = "<span style='color:#969896'> "+styledVal+" </span>"
+            }
+
+            // 对象 递归
+            if(typeof json[k] == "object"){
+
+                if(json[k]==null){
+                    // null值
+                    styledVal = "<span style='color:#aa3022'> null </span>"
+                }else{
+                    var eles;
+                    if(json instanceof Array){
+                        // 若当前元素为数组
+                        eles = i>=length ? styleition(json[k],null,true) : styleition(json[k],null,false);
+                    }else{
+                        // 否则
+                        eles = i>=length ? styleition(json[k],k,true) : styleition(json[k],k,false);
+                    }
+                    wrap.appendChild(eles);
+                    continue; // 防止Object元素重复添加
+                }
+            }
+
+            // 新建一行
+            var line = document.createElement("p");
+            // 设置新行缩进和行距
+            line.style.paddingLeft = indent;
+            line.style.margin = lineMargin+" 0";
+
+            // 设置新行值,此处要判断是否到了最后一个对象,来确定是否要加逗号
+            line.innerHTML = styledKey +styledVal+",<br>";
+            if(i>=length){
+                // 如果是最后,则需要去掉最后属性的逗号
+                line.innerHTML = line.innerHTML.replace(",","");
+            }
+            // 追加行
+            wrap.appendChild(line);
+        }
+        // 追加值区
+        styledEle.appendChild(wrap);
+        // 追加回括弧
+        styledEle.appendChild(rightq);
+        return styledEle;
     }
-    // 追加值区
-    styledEle.appendChild(wrap);
-    // 追加回括弧
-    styledEle.appendChild(rightq);
-    return styledEle;
-}
+};
